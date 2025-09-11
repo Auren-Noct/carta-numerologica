f119e0eabf058a937df81760de3ad683c4b8872e
Auren-Noct <LoneWolf4991@gmail.com>
2025-09-10 21:22:15 -0300

    Update README with TODO list and fix docker-compose volume naming

--- src/electron/main.ts
+++ src/electron/main.ts
@@ -1,7 +1,31 @@
 import { app, BrowserWindow } from "electron";
 import path from "path";
 
-app.on("ready", () => {
-  const win = new BrowserWindow({});
+const createWindow = () => {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    // Opción para ocultar la barra de menú
+    autoHideMenuBar: true,
+  })
+
   win.loadFile(path.join(app.getAppPath(), "/dist-react/index.html"));
-});
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    // Para macOS, si todas las ventanas están cerradas, se recrea una
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow()
+    }
+  })
+})
+
+// Cierra la aplicación cuando todas las ventanas estén cerradas, excepto en macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
