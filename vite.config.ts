f3af921ebd1cd4430c13de5e9b77617880532456
Auren-Noct <LoneWolf4991@gmail.com>
2025-09-27 21:45:57 -0300

    feat: integrate PWA for enhanced user experience
    - Added PWA plugin
    - Updated icons and theme color
    - Removed unnecessary favicon links

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,36 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png"],
+      manifest: {
+        name: "Calculadora Numerológica",
+        short_name: "NumCalc",
+        description: "Calculadora Numerológica basada en la Técnica Yoel.",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
   base: "/carta-numerologica/",
   server: {
     host: true,
